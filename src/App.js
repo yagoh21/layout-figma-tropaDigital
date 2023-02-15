6fa869ec463b1ee8f0befead95660c9cadcdfd7c
Yago Henrique <yagohfirmino19@gmail.com>
2023-02-15 05:13:21 -0300

    TropaDigital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+
+
+// eslint-disable-next-line 
+
 import './App.css';
+
+import Eu from './components/img/eu.png'
+import Tropadigital from './components/img/tropadigital.png'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  
+       
+
+      <div className='main'>
+       
+        <div className='center'>
+      
+        <img className='imagem1' src={Tropadigital}></img>
+    
+    
+        
+
+        
+        <div className='inicio' ><h3>Inicio</h3> </div>
+          
+
+
+        <div className='contatos1'><h3>Contatos</h3></div>
+          <div className='relatorio'><h3>Relatórios</h3></div>
+          <div className='contatos2'><h3>Contatos</h3></div>
+          <div className='contatos3'><h3>Contatos</h3></div>
+
+                  
+
+                    <div className='menu'>
+                    <div className='logo'>
+                      
+
+
+
+                    </div>
+                
+
+
+                      </div>
+                      <div className='bot'>
+                        <h2 className='titulo'> Olá Usuario</h2>
+                        <div className='linha'></div>
+                        <div className='borda1'>
+                      <img className='imagem2' display='display-flex' src={Eu}></img>
+                      <div className='calendario'></div> 
+                      
+                      <h1 className='texto1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  Duis posuere turpis...</h1>
+                      <p className='lorem'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  Duis posuere turpis...</p>
+                    </div>
+                      <div className='borda2'>
+                        <img className='imagem3' src={Eu}></img>
+            <h1 className='texto2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Duis posuere turpis...</h1>
+                      </div>
+                     
+                      <div className='borda3'>
+                        <img className='imagem4' src={Eu}></img>
+            <h1 className='texto3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Duis posuere turpis...</h1>
+            <p className='subtitulo1'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</p> 
+            <p className='titulo10'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</p>
+          </div>
+          
+                      <div className='borda4'>
+            <img className='imagem5' src={Eu}></img>
+            <h1 className='texto4'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</h1>
+            <p className='subtitulo2'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</p>   
+            
+          </div>
+
+          <div className='borda5'>
+            <img className='imagem6' src={Eu}></img>
+            <h1 className='texto5'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</h1>
+            <p className='subtitulo3'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</p>
+
+          </div>
+
+
+
+          
+          
+                    
+                    <div className='borda7'>
+                      <img className='imagem8' src={Eu}></img>
+                      <h1 className='texto7'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</h1>
+                      <p className='subtitulo'>Lorem ipsum dolor sit amet,consectetur adipiscing elit, Duis posuere turpis...</p>                    
+                    </div>
+
+                    </div>
+                   
+
+
+                    
+                  </div>
+                  
     </div>
+    
   );
 }
 
 export default App;
+
